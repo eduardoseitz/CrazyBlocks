03f1803cd42e587a69f6e9e9d134421ce6843d87
Eduardo Seitz <eduardoseitz0808@gmail.com>
2021-03-03 23:15:24 -0300

    Implemented parallax effect

---
+++ Block Breaker/Assets/Scripts/ParallaxController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class ParallaxController : MonoBehaviour
+{
+    public ParallaxLayer[] layers;
+
+    private Camera mainCamera;
+
+    private float cameraHorizontalSize;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera = Camera.main;
+
+        // Calculate screen ratio
+        cameraHorizontalSize = Camera.main.orthographicSize / 3 * 4;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Get horizontal mouse position
+        float mousePosition = mainCamera.ScreenToWorldPoint(Input.mousePosition).x;
+        float horizontalPosition = Mathf.Clamp(mousePosition, -cameraHorizontalSize, cameraHorizontalSize);
+
+        for (int i = 0; i < layers.Length; i++)
+        {
+            float newSpritePosition = horizontalPosition * -layers[i].maxDistance;
+            layers[i].sprite.position = new Vector3(newSpritePosition, layers[i].sprite.position.y, layers[i].sprite.position.z);
+        }
+    }
+}
+
+[System.Serializable]
+public class ParallaxLayer
+{
+    public string layerName;
+    public Transform sprite;
+    public float maxDistance = 1.2f;
+}
